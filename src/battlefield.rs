da5c147433179bbc7f97cd8f7ead51b5e476775e
Raven <sd5356@rit.edu>
2024-03-26 03:16:02 -0400

    Text for charge levels.

--- src/battlefield.rs
+++ src/battlefield.rs
@@ -23,12 +23,16 @@ const TURRET_HEAD_THICNESS: f32 = 2.5;
 const TURRET_HEAD_LENGTH: f32 = 75.0;
 const TURRET_ROTATION_SPEED: f32 = 1.0;
 
+const BULLET_TEXT_COLOR: Color = Color::BLACK;
+const BULLET_TEXT_FONT_SIZE: f32 = 24.0;
+
 // Z-index
 const TILE_Z: f32 = 10.0;
+const BULLET_TEXT_Z: f32 = 20.0;
 // Turret head is a child of turret, which inherits the z position as well, so the local z of the
 // head needs to be negative to put it behind the main turret.
 const TURRET_HEAD_Z: f32 = -1.0;
-const TURRET_Z: f32 = 12.0;
+const TURRET_Z: f32 = -1.0;
 
 // }}}
 
@@ -36,7 +40,7 @@ pub struct BattlefieldPlugin;
 impl Plugin for BattlefieldPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup)
-            .add_systems(Update, rotate_turret);
+            .add_systems(Update, (rotate_turret, update_charge_text));
     }
 }
 
@@ -77,6 +81,39 @@ impl TileBundle {
 }
 #[derive(Resource, Default, Clone)]
 struct TurretStopwatch(Stopwatch);
+#[derive(Component, Clone, Copy)]
+struct Charge(usize);
+impl Default for Charge {
+    fn default() -> Self {
+        Self(1)
+    }
+}
+#[derive(Bundle, Default)]
+struct TurretBundle {
+    charge: Charge,
+    text_bundle: Text2dBundle,
+    owner: Participant,
+}
+impl TurretBundle {
+    fn new(owner: Participant, x: f32, y: f32) -> Self {
+        Self {
+            owner,
+            charge: Default::default(),
+            text_bundle: Text2dBundle {
+                transform: Transform::from_xyz(x, y, BULLET_TEXT_Z),
+                text: Text::from_section(
+                    "",
+                    TextStyle {
+                        font: Default::default(),
+                        font_size: BULLET_TEXT_FONT_SIZE,
+                        color: BULLET_TEXT_COLOR,
+                    },
+                ),
+                ..default()
+            },
+        }
+    }
+}
 /// Marker to indicate the entity is a turret head.
 #[derive(Component)]
 struct TurretHead;
@@ -109,44 +146,29 @@ impl TurretHeadBundle {
     }
 }
 /// Component for a turret.
-#[derive(Component)]
-#[allow(dead_code)]
-struct Turret {
-    /// The direction that the barrel would be pointing in with an offset_angle of 0.
-    base_offset: f32,
-}
+#[derive(Component, Default)]
+struct BarrelOffset(f32);
 /// Component bundle for a turret.
-#[derive(Bundle)]
-struct TurretBundle {
+#[derive(Bundle, Default)]
+struct TurretPlatformBundle {
     /// Bevy rendering component used to display the ball.
     matmesh: MaterialMesh2dBundle<ColorMaterial>,
-    /// The game participant that owns this ball.
-    owner: Participant,
     marker: Sensor,
     collider: Collider,
-    /// Variables for the functionality of the turret.
-    turret: Turret,
+    barrel_offset: BarrelOffset,
 }
-impl TurretBundle {
-    fn new(
-        position: Vec2,
-        material: Handle<ColorMaterial>,
-        mesh: Mesh2dHandle,
-        owner: Participant,
-        base_offset: f32,
-    ) -> Self {
+impl TurretPlatformBundle {
+    fn new(material: Handle<ColorMaterial>, mesh: Mesh2dHandle, base_offset: f32) -> Self {
         Self {
-            owner,
             matmesh: MaterialMesh2dBundle {
+                transform: Transform::from_xyz(0.0, 0.0, TURRET_Z),
                 material,
                 mesh,
-                transform: Transform::from_translation(position.extend(TURRET_Z))
-                    .with_rotation(Quat::from_rotation_z(base_offset)),
                 ..default()
             },
             marker: Sensor,
             collider: Collider::ball(TURRET_RADIUS),
-            turret: Turret { base_offset },
+            barrel_offset: BarrelOffset(base_offset),
         }
     }
 }
@@ -190,54 +212,70 @@ fn setup(
             fn head_spawner(turret: &mut ChildBuilder) {
                 turret.spawn(TurretHeadBundle::new());
             }
+            let spawn_turret =
+                |parent: &mut ChildBuilder, material: Handle<ColorMaterial>, base_offset| {
+                    parent
+                        .spawn(TurretPlatformBundle::new(
+                            material,
+                            mesh.clone(),
+                            base_offset,
+                        ))
+                        .with_children(head_spawner);
+                };
             parent
                 .spawn(TurretBundle::new(
-                    Vec2::new(TURRET_POSITION, TURRET_POSITION),
-                    materials.a.clone(),
-                    mesh.clone(),
                     Participant::A,
-                    PI,
+                    TURRET_POSITION,
+                    TURRET_POSITION,
                 ))
-                .with_children(head_spawner);
+                .with_children(|parent| {
+                    spawn_turret(parent, materials.a.clone(), PI);
+                });
             parent
                 .spawn(TurretBundle::new(
-                    Vec2::new(-TURRET_POSITION, TURRET_POSITION),
-                    materials.b.clone(),
-                    mesh.clone(),
                     Participant::B,
-                    -FRAC_PI_2,
+                    -TURRET_POSITION,
+                    TURRET_POSITION,
                 ))
-                .with_children(head_spawner);
+                .with_children(|parent| {
+                    spawn_turret(parent, materials.b.clone(), -FRAC_PI_2);
+                });
             parent
                 .spawn(TurretBundle::new(
-                    Vec2::new(TURRET_POSITION, -TURRET_POSITION),
-                    materials.c.clone(),
-                    mesh.clone(),
                     Participant::C,
-                    FRAC_PI_2,
+                    TURRET_POSITION,
+                    -TURRET_POSITION,
                 ))
-                .with_children(head_spawner);
+                .with_children(|parent| {
+                    spawn_turret(parent, materials.c.clone(), FRAC_PI_2);
+                });
             parent
                 .spawn(TurretBundle::new(
-                    Vec2::new(-TURRET_POSITION, -TURRET_POSITION),
-                    materials.d.clone(),
-                    mesh.clone(),
                     Participant::D,
-                    0.0,
+                    -TURRET_POSITION,
+                    -TURRET_POSITION,
                 ))
-                .with_children(head_spawner);
+                .with_children(|parent| {
+                    spawn_turret(parent, materials.d.clone(), 0.0);
+                });
         });
 }
 fn rotate_turret(
     time: Res<Time>,
     mut stopwatch: ResMut<TurretStopwatch>,
-    mut turrets: Query<(&mut Transform, &Turret)>,
+    mut turrets: Query<(&mut Transform, &BarrelOffset)>,
 ) {
     stopwatch.0.tick(time.delta());
     let angle_offset = FRAC_PI_2
         - ((stopwatch.0.elapsed_secs() % PI * TURRET_ROTATION_SPEED) % PI - FRAC_PI_2).abs();
-    for (mut transform, turret) in &mut turrets {
-        *transform =
-            transform.with_rotation(Quat::from_rotation_z(turret.base_offset - angle_offset));
+    for (mut transform, &BarrelOffset(base_offset)) in &mut turrets {
+        *transform = transform.with_rotation(Quat::from_rotation_z(base_offset - angle_offset));
+    }
+}
+fn update_charge_text(
+    mut query: Query<(&mut Text, &Charge), Or<(Changed<Charge>, Added<Charge>)>>,
+) {
+    for (mut text, &Charge(charge)) in &mut query {
+        text.sections[0].value = charge.to_string();
     }
 }
